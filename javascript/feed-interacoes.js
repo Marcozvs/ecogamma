79a09ffb2991a524b528dc10cb2e968ecb789676
Marcos Vinícius da Silva <marcossampa.vinicius@gmail.com>
2022-05-15 11:54:25 -0300

    Fazendo o botão curtir e o comentar interar ao ser clicado

---
+++ javascript/feed-interacoes.js
@@ -0,0 +1,11 @@
+function curtir(){
+    let curtir = document.getElementById("botaoCurtir");
+    curtir.classList.remove("fa-regular");
+    curtir.classList.add("fa-solid");
+}
+
+function comentar(){
+    let curtir = document.getElementById("botaoComentar");
+    curtir.classList.remove("fa-regular");
+    curtir.classList.add("fa-solid");
+}
