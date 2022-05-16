63ca44644bac7bcca68e387e90159c8390ad9a59
Marcos Vinícius da Silva <marcossampa.vinicius@gmail.com>
2022-05-15 21:29:14 -0300

    Começando a mexer nas interações de curtir e comentar

--- javascript/feed-interacoes.js
+++ javascript/feed-interacoes.js
@@ -1,11 +1,21 @@
 function curtir(){
     let curtir = document.getElementById("botaoCurtir");
-    curtir.classList.remove("fa-regular");
-    curtir.classList.add("fa-solid");
+    let classeElement = document.getElementsByClassName("fa-heart")
+    
+    if (classeElement = "fa-heart"){
+        curtir.classList.remove("fa-regular");
+        curtir.classList.add("fa-solid");
+        curtir.style.color = "#34B132"
+    }else{
+        curtir.classList.remove("fa-solid");
+        curtir.classList.add("fa-regular");
+        curtir.style.color = "#5570FF"
+    }
 }
 
 function comentar(){
-    let curtir = document.getElementById("botaoComentar");
-    curtir.classList.remove("fa-regular");
-    curtir.classList.add("fa-solid");
+    let comentar = document.getElementById("botaoComentar");
+    comentar.classList.remove("fa-regular");
+    comentar.classList.add("fa-solid");
+    comentar.style.color = "#34B132"
 }
