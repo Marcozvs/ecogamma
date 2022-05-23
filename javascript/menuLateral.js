d0fd0f1c386cf4faff3cd554931969cc591d467c
Marcozvs <90426604+Marcozvs@users.noreply.github.com>
2022-05-23 17:48:57 -0300

    Terminando grid agora só recolar conteúdo

---
+++ javascript/menuLateral.js
@@ -0,0 +1,7 @@
+//função menu lateral
+function menuLateralOpen(){
+    document.getElementById("menuLateral").style.display = "flex";
+}
+function menuLateralClose(){
+    document.getElementById("menuLateral").style.display = "none";
+}
