a4db59c98ea27669ff987ca72a5b0e7ec115fa48
Marcozvs <90426604+Marcozvs@users.noreply.github.com>
2022-05-23 05:04:49 -0300

    aplicando grid

---
+++ javascript/comentar.js
@@ -0,0 +1,5 @@
+document.addEventListener("click", comentar);
+
+function comentar() {
+  document.getElementById("botaoComentar").style.color = "#34B132";
+}
---
+++ javascript/curtir.js
@@ -0,0 +1,5 @@
+document.addEventListener("click", curtir);
+
+function curtir() {
+  document.getElementById("botaoCurtir").style.color = "#34B132";
+}
