ab64b08543f1d36db70f93ebba0ee843b48231da
Marcozvs <90426604+Marcozvs@users.noreply.github.com>
2022-05-23 17:46:56 -0300

    Delete javascript directory

--- javascript/comentar.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("click", comentar);
-
-function comentar() {
-  document.getElementById("botaoComentar").style.color = "#34B132";
-}
--- javascript/curtir.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("click", curtir);
-
-function curtir() {
-  document.getElementById("botaoCurtir").style.color = "#34B132";
-}
