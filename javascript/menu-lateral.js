d05cc6808e66dde062fff68cb683cdef0f7fe02c
Marcozvs <90426604+Marcozvs@users.noreply.github.com>
2022-05-29 15:34:15 -0300

    Delete javascript directory

--- javascript/menu-lateral.js
+++
@@ -1,8 +0,0 @@
-//função menu lateral
-
-function menuLateralOpen(){
-    document.getElementById("menuLateral").style.display = "flex";
-}
-function menuLateralClose(){
-    document.getElementById("menuLateral").style.display = "none";
-}
