df62c3b8a7c0f2545ce2f33547a6246e4266326b
Leandro Silva <leandrosdc@outlook.com>
2022-06-02 15:29:32 -0300

    Começando o sistema de login
    Arquivos login.php e loginProcessa.php foram modificados.

--- estrutura/loginProcessa.php
+++ estrutura/loginProcessa.php
@@ -17,18 +17,46 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     </head>
     <body>
- 
         <section class="login">
-            <form action="" method="POST">
-                <label for="email">Email</label>
-                <input type="mail" placeholder="Insira seu email..." maxlength="100" minlength="1" required  name="email" id="email">
-                <label for="senha">Senha</label>
-                <input type="password" placeholder="Insira sua senha..." maxlength="100" minlength="1" required name="senha" id="senha">
-                <input type="submit">
-            </form>
+
         </section>
         <?php 
-        
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "ecogamma";
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+
+        //select
+        $sql = "SELECT senha, email FROM usuario";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "Email: " . $row["email"]. " - Senha: " . $row["senha"];
+        }
+        } else {
+        echo "0 results";
+        }
+
+
+        if ($senha!=$row["senha"]) {
+            # code...
+            header('Location: login.php');
+        } else {
+            # code...
+            header('Location: index.php');
+        }
         ?>
     </body>
 </html>
