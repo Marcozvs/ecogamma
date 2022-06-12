fb6b3cad423769e907903e7a433c5791ebf9dc5d
Marcos Vinícius da Silva <marcossampa.vinicius@gmail.com>
2022-06-12 13:20:11 -0300

    tirando post padrão do perfil

--- estrutura/perfil.php
+++ estrutura/perfil.php
@@ -177,39 +177,6 @@
         </div>
       </div>"
        ?>
-      <section class='container__post--perfil'>
-        <div class='container__post__perfil'>
-          <div class='container__post__perfil__foto'>
-            <img src='../imagens/perfil_default.svg' alt='Imagem do Perfil'>
-          </div>
-          <div class='container__post__perfil__dados'>
-            <p class='container__post__perfil__dados__nome nome__perfil'>Fulano de Tal</p>
-            <p class='container__post__perfil__dados__cargo cargo__perfil'>Profissional de Sei la</p>
-          </div>
-        </div>
-        <div class='container__post__conteudo'>
-          <p class='container__post__conteudo__texto'>25/05/22</p>
-          <p class='container__post__conteudo__texto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas iusto quo vero iure quidem, nobis eius quam quibusdam dolores similique! Architecto repellendus aliquid alias dolorum nihil provident sed ex cumque!</p>
-          <img src='../imagens/" . $imagem_Post . "' alt='imagem do conteúdo' class='container__post__conteudo__imagem'>
-        </div>
-        <div class='container__post__interacoes'>
-          <ul class='container__post__interacoes__lista'>
-            <li class='container__post__interacoes__lista__item'>
-              <span class='material-symbols-outlined container__menu__icone span--azul'>&#xe87d;</span>
-              <p>32</p>
-            </li>
-            <li class='container__post__interacoes__lista__item'>
-              <span class='material-symbols-outlined container__menu__icone span--azul'>&#xe0b9;</span>
-              <p>32</p>
-            </li>
-            <li class='container__post__interacoes__lista__item'>
-              <span class='material-symbols-outlined container__menu__icone span--azul'>&#xe163;</span>
-              <p>32</p>
-            </li>
-          </ul>
-        </div>
-      </section>
-    </div>
   </main>
   <script src="../manipulacao/manuLateral.js"></script>
 </body>
