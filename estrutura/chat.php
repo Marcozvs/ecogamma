f5e3a367a7ab165b7d5730b46d92d8f86fe71259
zululuga <leandrosdc@outlook.com>
2022-06-16 16:59:39 -0300

    alteracoes

--- estrutura/chat.php
+++ estrutura/chat.php
@@ -123,7 +123,7 @@
         </div>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum dolorem laudantium consectetur obcaecati doloribus voluptatum laboriosam, fugit officiis atque praesentium magnam vero aut totam cum nesciunt sunt dolore labore dignissimos!</p>
       </section>
-      <form action="#" class="container__form__chat">
+      <form action="./chat.php" class="container__form__chat">
         <textarea name="mensagem" id="mensagem" cols="30" rows="10" placeholder="Escreva sua mensagem aqui..." class="container__form__chat__texto"></textarea>
         <input type="submit" value="Enviar" class="botao__principal container__form__chat__botao">
       </form>
